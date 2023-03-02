59e4b6ecb2c8c53011ed02eb1cef599262f561f0
T3chnopath <josias.navasca@gmail.com>
2023-03-02 00:54:27 -0800

    Added comments to challenge 2

--- Challenges/Ch2/sum.c
+++ Challenges/Ch2/sum.c
@@ -1,5 +1,7 @@
 #include "sum.h"
 
+//uses static variable that is not on the stack
+//variable persists across function calls 
 int sum(int num) {
     static int runningTotal;
     runningTotal = runningTotal + num;
