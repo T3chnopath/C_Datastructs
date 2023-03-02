4bbf85e9dbe667f257dc631c10e810b43495091f
T3chnopath <josias.navasca@gmail.com>
2023-03-02 00:56:26 -0800

    Added comments to challenge 1

--- Challenges/Ch1/main.c
+++ Challenges/Ch1/main.c
@@ -1,15 +1,15 @@
 
-static double doubleVar;
-extern float floatVarGlobal;
+static double doubleVar;        //global static visible only in this file
+extern float floatVarGlobal;    //global visible to all files, uses floatVarGlobal from another file
 
-static int staticFunc(){
+static int staticFunc(){        //static function only visible to this file
     return 0;
 }
 
 
 int main() {
-    int intLocal;
-    static float floatVarLocal;
-    register int registerInt;
+    int intLocal;               //local variable
+    static float floatVarLocal; //static local variable which persists across function calls 
+    register int registerInt;   //*may* indicate that the variable is in a register
     return 0;
 }
